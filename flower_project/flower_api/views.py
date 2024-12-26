3cb0eccdbe14363dfc01559a5665515cc16f0a9a
Maya Eid <mayabaseleid@gmail.com>
2024-12-27 01:17:56 +0200

    machine learning: Add model loading logic

--- flower_project/flower_api/views.py
+++ flower_project/flower_api/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import tensorflow as tf
+import json
+
+# Load model and labels
+model = tf.keras.models.load_model('flower_model.h5')
+with open('class_labels.json', 'r') as f:
+    class_labels = json.load(f)
 
 @csrf_exempt
 def predict_flower(request):
